19b2e330e1504b898e65c2163573560ca11adf02
maisen35 <suzukixoxb5021@gmail.com>
2022-04-14 12:00:59 +0000

    [add]

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ApplicationRecord
   belongs_to :favorites
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
