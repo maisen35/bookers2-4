26530c014528248c74b6d85420ec356d859284ec
maisen35 <suzukixoxb5021@gmail.com>
2022-04-15 08:05:20 +0000

    [finish]fav

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followers' => 'relationships#followers'
+    get 'followings' => 'relationships#followings'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
