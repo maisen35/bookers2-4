ef1d4d07a455254f04161084a2a0a011b52bc41d
maisen35 <suzukixoxb5021@gmail.com>
2022-04-16 08:02:17 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
-  get "search" => 'searches#search'
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers'
     get 'followings' => 'relationships#followings'
   end
+  get "search" => 'searches#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
